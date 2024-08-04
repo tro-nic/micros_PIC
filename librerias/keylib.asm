b50d5080c1addbba2e934d4be3b68323fe765f0d
Angel Bautista (TRONIC) <38329747+tro-nic@users.noreply.github.com>
2024-08-04 22:57:23 +0200

    Update and rename keylib.asm to librerias/keylib.asm

--- librerias/keylib.asm
+++ librerias/keylib.asm
@@ -1,9 +1,9 @@
-;* Estas subrutinas realizan la inicializaci髇 y detecci髇 de un teclado
-;* tipo telef髇ico controlando las columnas con las salidas RB1,RB2 y RB3
+;* Estas subrutinas realizan la inicializaci贸n y detecci贸n de un teclado
+;* tipo telef贸nico controlando las columnas con las salidas RB1,RB2 y RB3
 ;* y los renglones con las entradas RB4,RB5,RB6 y RB7
 ;* con resistencias pull-up internas para evitar resistencias externas.
 ;*********************************************************************;
-;                  inicializaci髇 del puerto C:                      *;
+;                  inicializaci贸n del puerto C:                      *;
 ;*********************************************************************;
 	         org	0x440
 
@@ -16,7 +16,7 @@ initB    	CLRF      STATUS	 	;Selecciona Banco 0
 	        BCF       STATUS,RP0 		;regresa al Banco 0
 	        RETURN
 
-;Subrutina de Detecci髇 de tecla presionada: regresa w=0 si no hay tecla
+;Subrutina de Detecci贸n de tecla presionada: regresa w=0 si no hay tecla
 ;presionada y w=0xFF si hay alguna tecla presionada
 
 CONFIGURA       BCF       PORTC,0
@@ -24,13 +24,13 @@ CONFIGURA       BCF       PORTC,0
         	BCF       PORTC,2
         	BCF       PORTC,3
 	        
-DETEC           BTFSS     PORTC,7 	;lee rengl髇 1,2,3
+DETEC           BTFSS     PORTC,7 	;lee rengl贸n 1,2,3
 	        GOTO      rebo	        ;si tecla presionada limpia rebote
-	        BTFSS     PORTC,6 	;lee rengl髇 4,5,6
+	        BTFSS     PORTC,6 	;lee rengl贸n 4,5,6
 	        GOTO      rebo  	;si tecla presionada limpia rebote
-	        BTFSS     PORTC,5	 ;lee rengl髇 7,8,9
+	        BTFSS     PORTC,5	 ;lee rengl贸n 7,8,9
 	        GOTO      rebo  	;si tecla presionada limpia rebote
-	        BTFSS     PORTC,4 	;lee rengl髇 *,0,#
+	        BTFSS     PORTC,4 	;lee rengl贸n *,0,#
 	        GOTO      rebo   	;si tecla presionada limpia rebote
 	        RETLW     0x0   	;no hbo tecla presionada retorna con w=0
 
@@ -40,13 +40,13 @@ rebo     	CALL      TEMPO1	;pausa de 50 milisegundos
 ;*          LEE FILAS             *;
 ;**********************************;
 
-                BTFSS     PORTC,7	;lee rengl髇 1,2,3
+                BTFSS     PORTC,7	;lee rengl贸n 1,2,3
 	        RETLW     0xFF   	;tecla presionada, retorna con w=0xFF
-	        BTFSS     PORTC,6 	;lee rengl髇 4,5,6
+	        BTFSS     PORTC,6 	;lee rengl贸n 4,5,6
 	        RETLW     0xFF          ;tecla presionada, retorna con w=0xFF
-	        BTFSS     PORTC,5 	;lee rengl髇 7,8,9
+	        BTFSS     PORTC,5 	;lee rengl贸n 7,8,9
 	        RETLW     0xFF  	;tecla presionada, retorna con w=0xFF
-	        BTFSS     PORTC,4 	;lee rengl髇 *,0,#
+	        BTFSS     PORTC,4 	;lee rengl贸n *,0,#
 	        RETLW     0xFF  	;tecla presionada, retorna con w=0xFF
 	        RETLW     0x00  	;falsa alarma retorna con w=0
 
@@ -57,33 +57,33 @@ rebo     	CALL      TEMPO1	;pausa de 50 milisegundos
 cod1     	MOVLW    0xF7   	;desactiva todas las columnas
         	MOVWF    PORTC  	;y activa la columna 1,4,7,*
         	BTFSS    PORTC,7 	;Es la tecla 1?
-        	RETLW    '1'    	;retorna c骴igo del '1'
+        	RETLW    '1'    	;retorna c贸digo del '1'
         	BTFSS    PORTC,6	 ;Es la tecla 4?
-        	RETLW    '4'     	 ;retorna c骴igo del '4'
+        	RETLW    '4'     	 ;retorna c贸digo del '4'
 	        BTFSS    PORTC,5	 ;Es la tecla 7?
-	        RETLW    '7'	        ;retorna c骴igo del '7'
+	        RETLW    '7'	        ;retorna c贸digo del '7'
         	BTFSS    PORTC,4 	;Es la tecla *?
-        	RETLW    '*'    	;Retorna c骴igo del '*'
+        	RETLW    '*'    	;Retorna c贸digo del '*'
         	MOVLW    0xFB   	;desactiva todas las columnas
           	MOVWF    PORTC  	;y activa la columna 2,5,8,0
         	BTFSS    PORTC,7 	;Es la tecla 2?
-        	RETLW    '2'    	;retorna c骴igo del '2'
+        	RETLW    '2'    	;retorna c贸digo del '2'
         	BTFSS    PORTC,6 	;Es la tecla 5?
-        	RETLW    '5'    	;retorna c骴igo del '5'
+        	RETLW    '5'    	;retorna c贸digo del '5'
         	BTFSS    PORTC,5 	;Es la tecla 8?
-        	RETLW    '8'    	;retorna c骴igo del '8'
+        	RETLW    '8'    	;retorna c贸digo del '8'
         	BTFSS    PORTC,4 	;Es la tecla 0?
-        	RETLW    '0'    	;Retorna c骴igo del '0'
+        	RETLW    '0'    	;Retorna c贸digo del '0'
         	MOVLW    0xFD   	;desactiva todas las columnas
         	MOVWF    PORTC  	;y activa la columna 3,6,9,#
         	BTFSS    PORTC,7 	;Es la tecla 3?
-        	RETLW    '3'    	;retorna c骴igo del '3'
+        	RETLW    '3'    	;retorna c贸digo del '3'
         	BTFSS    PORTC,6 	;Es la tecla 6?
-        	RETLW    '6'    	;retorna c骴igo del '6'
+        	RETLW    '6'    	;retorna c贸digo del '6'
         	BTFSS    PORTC,5 	;Es la tecla 9?
-                RETLW    '9'    	;retorna c骴igo del '9'
+                RETLW    '9'    	;retorna c贸digo del '9'
         	BTFSS    PORTC,4	 ;Es la tecla #?
-        	RETLW    '#'    	;Retorna c骴igo del '#'
+        	RETLW    '#'    	;Retorna c贸digo del '#'
         	
                 RETLW    0x00   	;falsa alarma, no hay tecla presionada
 
